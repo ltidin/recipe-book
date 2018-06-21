38fd11fb4af115a11ff02ab0e2cdac90943946fa
ltidin <viachaslav.rutkovskiy@gmail.com>
2018-06-21 15:43:19 +0300

    finished shopping-list template driven approach forms

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -36,4 +36,10 @@ export class ShoppingListService {
     this.ingredients[index] = updIngredient;
     this.ingredientsChanged.next(this.ingredients.slice());
   }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1)
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
   }
